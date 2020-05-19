c88e6c94aa354b9ce2fe24499863717f42c1ea07
Pajkic-lab <markopajkic@ymail.com>
2020-05-19 21:16:58 +0200

    changing server

--- server.js
+++ server.js
@@ -18,8 +18,7 @@ app.use('/auth', require('./routes/auth'))
 app.use('/photos', require('./routes/photos'))
 
 
-console.log(process.env.TOKEN)
-
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
