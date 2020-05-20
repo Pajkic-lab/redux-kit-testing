cc35cfa7aa47aa90c4a9a9525c8f051ec806ea15
Pajkic-lab <markopajkic@ymail.com>
2020-05-20 14:13:44 +0200

    dep

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 require('dotenv').config()
 const cors = require('cors')
 const db = require('./db/database')
+const path = require('path')
 
 
 
@@ -17,7 +18,13 @@ app.use('/users', require('./routes/users'))
 app.use('/auth', require('./routes/auth'))
 app.use('/photos', require('./routes/photos'))
 
-
+//serve static if in production
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res)=> {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
